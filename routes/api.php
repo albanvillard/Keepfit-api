1b8f24250d27f66f21b7b9d307e86c1605ee7513
villard <alban.villard@saint-benigne.fr>
2026-04-29 22:28:48 +0200

    nouvelle route et modification model challenge

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChallengeController;
 use App\Http\Controllers\ClasseChallengeController;
+use App\Http\Controllers\ExerciseController; // <-- N'oublie pas d'importer ce contrôleur !
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// L'URL sera : POST http://127.0.0.1:8000/api/challenges
+// --- ROUTES POUR LA RÉALISATION 1 (Affectation) ---
+Route::post('/affectations', [ClasseChallengeController::class, 'store']);
+
+// --- ROUTES POUR LA RÉALISATION 2 (Création de défi) ---
+
+// 1. Recevoir les données du formulaire pour créer le défi
 Route::post('/challenges', [ChallengeController::class, 'store']);
 
-
-
-Route::post('/affectations', [ClasseChallengeController::class, 'store']);
+// 2. Envoyer la liste des exercices pour le menu déroulant du Front-End React
+Route::get('/exercises', [ExerciseController::class, 'index']);
